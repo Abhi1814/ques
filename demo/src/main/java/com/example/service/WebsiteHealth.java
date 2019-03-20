687102a42913a8b497b60ffac46d0741b361926d
Abhishek <abhishekpal.pal2@gmail.com>
2019-03-20 12:48:53 +0530

    Update WebsiteHealth.java

--- demo/src/main/java/com/example/service/WebsiteHealth.java
+++ demo/src/main/java/com/example/service/WebsiteHealth.java
@@ -8,7 +8,7 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class WebsiteHealth implements HealthIndicator {
 
 
